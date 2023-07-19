977d900bc7b919289386b26bd41455e8e24d6c62
devjun10 <devjun1023@gmail.com>
2023-07-19 19:22:01 +0900

    chore: 스키마 추가
    - 예약 스키마 추가
    - 필드 설명 변경(취소 이유 -> 취소 사유)

--- order/src/main/resources/data/schema.sql
+++ order/src/main/resources/data/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE `order`
     user_id          BIGINT                                                   NOT NULL COMMENT '사용자 PK',
     deposit_deadline DATETIME(6)                                              NOT NULL COMMENT '입금 마감일',
     order_status     ENUM ('PAYMENT_REQUEST', 'COMPLETE', 'CANCEL', 'REFUND') NOT NULL COMMENT '주문 상태',
-    refund_reason    VARCHAR(1000)                                            NOT NULL COMMENT '환불 이유',
+    refund_reason    VARCHAR(1000)                                            NOT NULL COMMENT '환불 사유',
     created_by       BIGINT                                                   NOT NULL COMMENT '생성자',
     create_at        DATETIME(6)                                              NOT NULL COMMENT '생성일',
     last_modified_by BIGINT                                                   NULL COMMENT '최종 수정자',
@@ -29,3 +29,21 @@ CREATE TABLE order_line
     CONSTRAINT fk_order_line_order_id
         FOREIGN KEY (order_id) REFERENCES `order` (id)
 ) ENGINE = InnoDB COMMENT '주문 상세';
+
+CREATE TABLE reservation
+(
+    id                 BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL COMMENT '예약 PK',
+    user_id            BIGINT                            NOT NULL COMMENT '사용자 PK',
+    study_cafe_id      BIGINT                            NOT NULL COMMENT '스터디 카페 PK',
+    product_id         BIGINT                            NOT NULL COMMENT '상품 PK',
+    seat_id            BIGINT                            NOT NULL COMMENT '좌석 PK',
+    start_time         DATETIME(6)                       NOT NULL COMMENT '시작 시간',
+    end_time           DATETIME(6)                       NOT NULL COMMENT '끝나는 시간',
+    reservation_status ENUM ('RESERVED', 'CANCEL')       NOT NULL COMMENT '예약 상태',
+    cancel_reason      VARCHAR(1000)                     NULL COMMENT '취소 사유',
+    created_by         BIGINT                            NOT NULL COMMENT '생성자',
+    create_at          DATETIME(6)                       NOT NULL COMMENT '생성일',
+    last_modified_by   BIGINT                            NULL COMMENT '최종 수정자',
+    last_modified_at   DATETIME(6)                       NULL COMMENT '최종 수정일',
+    deleted            BIT                               NOT NULL COMMENT '삭제 유무'
+) ENGINE = InnoDB COMMENT '예약';
