54088652a914ba2868f4df59a9e2eba2a4d4f85c
devjun10 <devjun1023@gmail.com>
2023-07-21 23:25:39 +0900

    test: 주문 문서화 테스트 작성
    - 공통 어노테이션 정의

---
+++ order/src/test/java/project/swithme/order/test/order/documentation_test/save/OrderCreateDocumentationTest.java
@@ -0,0 +1,57 @@
+package project.swithme.order.test.order.documentation_test.save;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.restdocs.restassured.RestDocumentationFilter;
+import project.swithme.order.core.web.order.presentation.request.OrderRequest;
+import project.swithme.order.test.IntegrationTestBase;
+
+import java.math.BigDecimal;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.document;
+import static project.swithme.order.test.order.documentation_test.snippet.OrderSnippet.ORDER_CREATE_REQUEST_FIELD_DESCRIPTOR;
+import static project.swithme.order.test.order.documentation_test.snippet.OrderSnippet.ORDER_CREATE_RESPONSE;
+
+@DisplayName("[DocumentationTest] 주문 생성 API 테스트")
+class OrderCreateDocumentationTest extends IntegrationTestBase {
+
+    @Test
+    @DisplayName("주문이 성공하면 201 CREATED가 반환된다.")
+    void order_create_test() throws Exception {
+        OrderRequest request = createOrderCreateRequest();
+
+        given(this.specification)
+                .filters(documents)
+
+                .when()
+                .contentType(JSON)
+                .body(objectMapper.writeValueAsString(request))
+                .post("/api/orders")
+
+                .then()
+                .statusCode(equalTo(201))
+                .body(notNullValue())
+                .log()
+                .all();
+    }
+
+    private OrderRequest createOrderCreateRequest() {
+        return new OrderRequest(
+                1L,
+                1L,
+                BigDecimal.valueOf(100_000),
+                1L,
+                BigDecimal.valueOf(30_000)
+        );
+    }
+
+    RestDocumentationFilter documents = document(
+            "{class_name}/{method_name}/",
+            ORDER_CREATE_REQUEST_FIELD_DESCRIPTOR,
+            ORDER_CREATE_RESPONSE
+    );
+}
