f1beeaa4017c71233e6651af3da6a90fb3e1e55b
devjun10 <devjun1023@gmail.com>
2023-07-25 17:58:16 +0900

    feat: Caffeine 캐시 적용

--- order/src/main/java/project/swithme/order/SwithMeOrderApplication.java
+++ order/src/main/java/project/swithme/order/SwithMeOrderApplication.java
@@ -2,8 +2,10 @@ package project.swithme.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 public class SwithMeOrderApplication {
