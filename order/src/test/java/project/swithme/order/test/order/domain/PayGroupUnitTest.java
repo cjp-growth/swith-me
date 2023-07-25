5413cbb28739888765741a5e7289e1191a76484b
devjun10 <devjun1023@gmail.com>
2023-07-25 17:54:19 +0900

    test: 결제 그룹/타입 리스트 단위 테스트 작성

---
+++ order/src/test/java/project/swithme/order/test/order/domain/PayGroupUnitTest.java
@@ -0,0 +1,21 @@
+package project.swithme.order.test.order.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import project.swithme.order.core.domain.order.entity.PayGroup;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static project.swithme.order.core.domain.order.entity.PayGroup.findPayTypes;
+
+@DisplayName("[UnitTest] 결제 그룹 단위 테스트")
+class PayGroupUnitTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"CASH", "CARD"})
+    @DisplayName("올바른 결제 그룹을 입력하면 결제 수단 리스트가 나온다.")
+    void pay_type_list_search_test(String parameter) {
+        PayGroup payGroup = PayGroup.valueOf(parameter);
+        assertFalse(findPayTypes(payGroup).isEmpty());
+    }
+}
