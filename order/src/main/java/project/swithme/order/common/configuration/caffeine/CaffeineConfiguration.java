52185d3745f6dada50bef5280d6b6132d1bb8cf7
devjun10 <devjun1023@gmail.com>
2023-07-25 17:53:32 +0900

    feat: Caffeine 캐시 설정 추가

---
+++ order/src/main/java/project/swithme/order/common/configuration/caffeine/CaffeineConfiguration.java
@@ -0,0 +1,26 @@
+package project.study.caffeine.common.configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static java.util.concurrent.TimeUnit.DAYS;
+
+@Configuration
+public class CaffeineConfiguration {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(1, DAYS);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+}
