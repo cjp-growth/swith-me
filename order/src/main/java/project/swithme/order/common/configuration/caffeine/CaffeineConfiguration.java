ae6e2e6450893cdd3db5dfec65ede683d0b891de
devjun10 <devjun1023@gmail.com>
2023-07-26 10:20:32 +0900

    refactor: @EnableJpaAuditing 위치 이동
    - 한 곳에서 설정을 관리하기 위해 Caffeine 설정 클래스로 어노테이션 이동

--- order/src/main/java/project/swithme/order/common/configuration/caffeine/CaffeineConfiguration.java
+++ order/src/main/java/project/swithme/order/common/configuration/caffeine/CaffeineConfiguration.java
@@ -1,13 +1,15 @@
-package project.study.caffeine.common.configuration;
+package project.swithme.order.common.configuration.caffeine;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import static java.util.concurrent.TimeUnit.DAYS;
 
+@EnableCaching
 @Configuration
 public class CaffeineConfiguration {
 
