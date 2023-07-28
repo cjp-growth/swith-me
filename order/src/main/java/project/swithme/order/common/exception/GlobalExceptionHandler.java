dcbc27d0850a812ee97f8c4f10db0e5281eb63af
devjun10 <devjun1023@gmail.com>
2023-07-29 05:17:35 +0900

    refactor: Log 메시지를 남기도록 추가
    - @Slf4j 어노테이션 추가

--- order/src/main/java/project/swithme/order/common/exception/GlobalExceptionHandler.java
+++ order/src/main/java/project/swithme/order/common/exception/GlobalExceptionHandler.java
@@ -1,23 +1,27 @@
 package project.swithme.order.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import project.swithme.order.common.exception.response.ErrorResponse;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<ErrorResponse> catchTest(DomainException domainException) {
         ErrorResponse data = new ErrorResponse(domainException);
+        log.error("Error: {}", data);
         return new ResponseEntity<>(data, domainException.getStatus());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> catchUnresolvedException() {
         ErrorResponse data = ErrorResponse.ofServer();
+        log.error("Error: {}", data);
         return new ResponseEntity<>(data, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
