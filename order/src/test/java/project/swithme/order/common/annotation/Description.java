c9fb1d3e28b48440731b02f643b51ff79f234cc5
devjun10 <devjun1023@gmail.com>
2023-07-31 15:32:03 +0900

    test: 테스트 설명 어노테이션 추가

---
+++ order/src/test/java/project/swithme/order/common/annotation/Description.java
@@ -0,0 +1,10 @@
+package project.swithme.order.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface Description {
+
+    String content() default "";
+}
