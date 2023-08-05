8fd5e72aadcdf81c1c5222d8fd7f235e3b1f18c7
devjun10 <devjun1023@gmail.com>
2023-08-05 16:02:16 +0900

    refactor: 예외 시 로그 포맷 리팩토링
    - 현재 InvalidParameterException에서만 제대로 로깅하도록 리팩토링

--- order/src/main/java/project/swithme/order/common/exception/GlobalExceptionHandler.java
+++ order/src/main/java/project/swithme/order/common/exception/GlobalExceptionHandler.java
@@ -1,31 +1,75 @@
 package project.swithme.order.common.exception;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import project.swithme.order.common.aop.log.ErrorLog;
+import project.swithme.order.common.aop.log.Field;
+import project.swithme.order.common.exception.error.CommonCodeAndMessage;
 import project.swithme.order.common.response.ErrorResponse;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final String TRACE_ID = "traceId";
+
     @ExceptionHandler(DomainException.class)
-    public ResponseEntity<ErrorResponse> catchTest(DomainException domainException) {
+    public ResponseEntity<ErrorResponse> catchDomainException(DomainException domainException) {
         ErrorResponse data = createResponse(domainException);
-        log.error("Error: {}", data);
+        log.error("Error: {}", createResponse(domainException));
         return new ResponseEntity<>(data, domainException.getStatus());
+    }
+
+    // TODO. 미해결
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public void catchHttpMessageNotReadableException(
+        HttpMessageNotReadableException httpMessageNotReadableException
+    ) {
+    }
+
+    @ExceptionHandler(InvalidParameterException.class)
+    public ResponseEntity<ErrorResponse> catchInvalidParameterException(
+        InvalidParameterException invalidParameterException
+    ) {
+        ErrorLog errorLog = createLog(
+            CommonCodeAndMessage.BAD_REQUEST,
+            invalidParameterException.getFields()
+        );
+        log.error("[### PAYMENT_ERROR] -----x> traceId: {}, {}", getTraceId(), errorLog);
+        return new ResponseEntity<>(ErrorResponse.ofBadRequest(), BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> catchUnresolvedException() {
         ErrorResponse data = ErrorResponse.ofServer();
-        log.error("Error: {}", data);
+        log.error("### PAYMENT_ERROR -----x> traceId: {}, {}", getTraceId(), data);
         return new ResponseEntity<>(data, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private ErrorResponse createResponse(DomainException domainException) {
         return new ErrorResponse(domainException.getCodeAndMessage());
+    }
+
+    private ErrorLog createLog(
+        CommonCodeAndMessage codeAndMessage,
+        List<Field> fields
+    ) {
+        return new ErrorLog(
+            codeAndMessage.name(),
+            codeAndMessage.getStatusCode(),
+            codeAndMessage.getKrErrorMessage(),
+            fields
+        );
+    }
+
+    private String getTraceId() {
+        return MDC.get(TRACE_ID);
     }
 }
