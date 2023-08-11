15e43413c5528139037408f5ad09578fb9adf8d1
devjun10 <devjun1023@gmail.com>
2023-08-11 18:01:50 +0900

    feat, refactor: API 스펙 불일치 에러 위치 이동
    - 공통 예외로 처리

--- swith-me/support/src/main/java/project/study/support/codeandmessage/common/CommonErrorCodeAndMessage.java
+++ swith-me/support/src/main/java/project/study/support/codeandmessage/common/CommonErrorCodeAndMessage.java
@@ -4,6 +4,7 @@ import project.study.support.codeandmessage.CodeAndMessage;
 
 public enum CommonErrorCodeAndMessage implements CodeAndMessage {
     INVALID_REQUEST(400, "올바르지 않은 요청입니다."),
+    API_SPEC_UN_MATCHED(400, "외부 서버 API 스펙과 매핑을 확인해주세요."),
     INTERNAL_SERVER_ERROR(500, "서버 내부 오류입니다."),
     BAD_GATEWAY(502, "외부 서버와 통신하는 과정에서 오류가 발생했습니다.");
 
