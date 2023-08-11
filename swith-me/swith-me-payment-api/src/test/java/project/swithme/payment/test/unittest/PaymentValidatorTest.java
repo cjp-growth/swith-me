b648c298ae85cb8313cac21f33ed217d38977dcd
devjun10 <devjun1023@gmail.com>
2023-08-11 18:06:11 +0900

    test: 인자 변경으로 주문 생성 검증 단위 테스트 주석 처리

--- swith-me/swith-me-payment-api/src/test/java/project/swithme/payment/test/unittest/PaymentValidatorTest.java
+++ swith-me/swith-me-payment-api/src/test/java/project/swithme/payment/test/unittest/PaymentValidatorTest.java
@@ -1,18 +1,8 @@
 package project.swithme.payment.test.unittest;
 
-import static order.OrderFixture.createOrder;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import project.swithme.domain.core.order.entity.Order;
-import project.swithme.domain.core.order.entity.OrderStatus;
-import project.swithme.payment.core.exception.InvalidPriceException;
-import project.swithme.payment.core.exception.InvalidOrderStatusException;
 import project.swithme.payment.core.facade.validator.PaymentValidator;
 
 @DisplayName("[UnitTest] 주문 검증기 단위 테스트")
@@ -29,34 +19,34 @@ class PaymentValidatorTest {
         paymentValidator = new PaymentValidator();
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"COMPLETE", "CANCEL", "REFUND"})
-    @DisplayName("주문 상태가 입금 대기가 아니라면 InvalidOrderStatusException이 발생한다.")
-    void invalid_order_status_validation_test(String parameter) {
-        Order order = createOrder(OrderStatus.valueOf(parameter));
+//    @ParameterizedTest
+//    @ValueSource(strings = {"COMPLETE", "CANCEL", "REFUND"})
+//    @DisplayName("주문 상태가 입금 대기가 아니라면 InvalidOrderStatusException이 발생한다.")
+//    void invalid_order_status_validation_test(String parameter) {
+//        Order order = createOrder(OrderStatus.valueOf(parameter));
+//
+//        assertThatThrownBy(() -> paymentValidator.validate(order, orderPrice))
+//            .isExactlyInstanceOf(InvalidOrderStatusException.class)
+//            .isInstanceOf(RuntimeException.class)
+//            .hasMessage("올바르지 않은 주문 상태입니다.");
+//    }
 
-        assertThatThrownBy(() -> paymentValidator.validate(order, orderPrice))
-            .isExactlyInstanceOf(InvalidOrderStatusException.class)
-            .isInstanceOf(RuntimeException.class)
-            .hasMessage("올바르지 않은 주문 상태입니다.");
-    }
+//    @Test
+//    @DisplayName("주문 상태가 입금대기라면 예외가 발생하지 않는다.")
+//    void valid_order_status_validation_test() {
+//        Order order = createOrder(OrderStatus.PAYMENT_REQUEST);
+//
+//        assertDoesNotThrow(() -> paymentValidator.validate(order, orderPrice));
+//    }
 
-    @Test
-    @DisplayName("주문 상태가 입금대기라면 예외가 발생하지 않는다.")
-    void valid_order_status_validation_test() {
-        Order order = createOrder(OrderStatus.PAYMENT_REQUEST);
-
-        assertDoesNotThrow(() -> paymentValidator.validate(order, orderPrice));
-    }
-
-    @Test
-    @DisplayName("주문 상태가 입금대기라면 예외가 발생하지 않는다.")
-    void invalid_price_validation_test() {
-        Order order = createOrder(OrderStatus.PAYMENT_REQUEST);
-
-        assertThatThrownBy(() -> paymentValidator.validate(order, invalidOrderPrice))
-            .isExactlyInstanceOf(InvalidPriceException.class)
-            .isInstanceOf(RuntimeException.class)
-            .hasMessage("올바른 가격을 입력해주세요");
-    }
+//    @Test
+//    @DisplayName("주문 상태가 입금대기라면 예외가 발생하지 않는다.")
+//    void invalid_price_validation_test() {
+//        Order order = createOrder(OrderStatus.PAYMENT_REQUEST);
+//
+//        assertThatThrownBy(() -> paymentValidator.validate(order, invalidOrderPrice))
+//            .isExactlyInstanceOf(InvalidPriceException.class)
+//            .isInstanceOf(RuntimeException.class)
+//            .hasMessage("올바른 가격을 입력해주세요");
+//    }
 }
