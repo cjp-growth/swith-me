a1bee9b3f5697c63abf9e9043fa833c23b88e857
devjun10 <devjun1023@gmail.com>
2023-08-11 11:52:37 +0900

    feat: 토스 결제 과정에서 발생하는 외부 CodeAndMessage 정의

---
+++ swith-me/support/src/main/java/project/study/support/codeandmessage/out/toss/TossPaymentApprovalCodeAndMessage.java
@@ -0,0 +1,50 @@
+package project.study.support.codeandmessage.out.toss;
+
+import project.study.support.codeandmessage.CodeAndMessage;
+import project.study.support.codeandmessage.PaymentCodeAndMessage;
+
+public enum TossPaymentApprovalCodeAndMessage implements PaymentCodeAndMessage {
+    ALREADY_PROCESSED_PAYMENT(
+        409,
+        "이미 처리된 결제 입니다.",
+        "This is a payment that has already been processed."
+    ),
+    NOT_FOUND_PAYMENT_SESSION(
+        410,
+        "결제 시간이 만료되어 결제 진행 데이터가 존재하지 않습니다.",
+        "Payment session does not exist because the session time has expired."
+    );
+
+    private final int statusCode;
+    private final String krErrorMessage;
+    private final String engErrorMessage;
+
+    TossPaymentApprovalCodeAndMessage(
+        int statusCode,
+        String krErrorMessage,
+        String engErrorMessage
+    ) {
+        this.statusCode = statusCode;
+        this.krErrorMessage = krErrorMessage;
+        this.engErrorMessage = engErrorMessage;
+    }
+
+    public static CodeAndMessage findCodeAndMessage(String errorCode) {
+        return TossPaymentApprovalCodeAndMessage.valueOf(errorCode);
+    }
+
+    @Override
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    @Override
+    public String getKrErrorMessage() {
+        return krErrorMessage;
+    }
+
+    @Override
+    public String getEngErrorMessage() {
+        return engErrorMessage;
+    }
+}
