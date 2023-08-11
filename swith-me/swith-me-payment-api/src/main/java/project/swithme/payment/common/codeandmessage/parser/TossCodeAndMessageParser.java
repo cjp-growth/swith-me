949f25fb5766b952ada573bc510285a0a975dbaa
devjun10 <devjun1023@gmail.com>
2023-08-11 11:50:59 +0900

    feat: CodeAndMessageParser 계층도 구현
    - 결제(승인/취소) 과정에서 발생하는 에러 메시지를 파싱하기 위한 클래스 계층도 구현
     * 토스 뿐 아니라 다른 결제 수단에서도 사용할 수 있기 때문

---
+++ swith-me/swith-me-payment-api/src/main/java/project/swithme/payment/common/codeandmessage/parser/TossCodeAndMessageParser.java
@@ -0,0 +1,74 @@
+package project.swithme.payment.common.codeandmessage.parser;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import project.study.support.codeandmessage.CodeAndMessage;
+import project.study.support.codeandmessage.out.toss.TossPaymentApprovalCodeAndMessage;
+import project.study.support.codeandmessage.out.toss.TossPaymentCancelCodeAndMessage;
+import project.study.support.codeandmessage.parser.CodeAndMessageParser;
+import project.study.support.exception.ErrorMessageParseException;
+import project.swithme.payment.core.out.port.adapter.response.failure.TossPaymentErrorResponse;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TossCodeAndMessageParser implements CodeAndMessageParser {
+
+    private static final String DELIMETER = ": ";
+    private static final int CODE_AND_MESSAGE = 1;
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public CodeAndMessage parseRequestApprovalFailureMessage(String errorMessage) {
+        try {
+            TossPaymentErrorResponse[] responses = getResponse(errorMessage);
+            return findApprovalCodeAndMessageByCode(Arrays.asList(responses));
+        } catch (
+            IndexOutOfBoundsException | JsonProcessingException | IllegalArgumentException exception
+        ) {
+            throw new ErrorMessageParseException(exception.getMessage());
+        }
+    }
+
+    @Override
+    public CodeAndMessage parsePaymentCancelFailureMessage(String errorMessage) {
+        try {
+            TossPaymentErrorResponse[] responses = getResponse(errorMessage);
+            return findFailureCodeAndMessageByCode(Arrays.asList(responses));
+        } catch (
+            IndexOutOfBoundsException | JsonProcessingException | IllegalArgumentException exception
+        ) {
+            throw new ErrorMessageParseException(exception.getMessage());
+        }
+    }
+
+    private TossPaymentErrorResponse[] getResponse(
+        String errorMessage
+    ) throws JsonProcessingException {
+        String[] errorInforArray = errorMessage.split(DELIMETER);
+        return objectMapper.readValue(
+            errorInforArray[CODE_AND_MESSAGE],
+            TossPaymentErrorResponse[].class
+        );
+    }
+
+    private CodeAndMessage findApprovalCodeAndMessageByCode(List<TossPaymentErrorResponse> errors) {
+        return errors.stream()
+            .map(error -> TossPaymentApprovalCodeAndMessage.findCodeAndMessage(error.getCode()))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("매핑된 에러 메시지를 찾을 수 없습니다."));
+    }
+
+    private CodeAndMessage findFailureCodeAndMessageByCode(List<TossPaymentErrorResponse> errors) {
+        return errors.stream()
+            .map(error -> TossPaymentCancelCodeAndMessage.findCodeAndMessage(error.getCode()))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("매핑된 에러 메시지를 찾을 수 없습니다."));
+    }
+}
