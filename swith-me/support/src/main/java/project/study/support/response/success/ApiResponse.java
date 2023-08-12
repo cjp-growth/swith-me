4b22fba21249356b2b0c825a102caf68899589cb
devjun10 <devjun1023@gmail.com>
2023-08-13 08:29:07 +0900

    refactor: API 응답 생성자 추가
    - Data가 null일 경우 생성자 추가
    - 결제 취소 API 반환 타입 변경

--- swith-me/support/src/main/java/project/study/support/response/success/ApiResponse.java
+++ swith-me/support/src/main/java/project/study/support/response/success/ApiResponse.java
@@ -6,12 +6,22 @@ import project.study.support.codeandmessage.CodeAndMessage;
 
 public class ApiResponse<T> extends ResponseEntity<ResponsePayload<T>> {
 
+    public static Object EMPTY_BODY = null;
+
     public ApiResponse(
         T data,
         HttpStatus status,
         String message
     ) {
         super(new ResponsePayload<>(data, status, message), status);
+    }
+
+    public static <T> ApiResponse<T> of(CodeAndMessage codeAndMessage) {
+        return new ApiResponse<>(
+            null,
+            HttpStatus.valueOf(codeAndMessage.getStatusCode()),
+            codeAndMessage.getKrErrorMessage()
+        );
     }
 
     public static <T> ApiResponse<T> of(
