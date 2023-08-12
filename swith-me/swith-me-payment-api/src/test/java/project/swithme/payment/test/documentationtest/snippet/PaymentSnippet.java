6ee4a886ec8cf6290e52a9876de1e1d03c13e94e
devjun10 <devjun1023@gmail.com>
2023-08-13 08:29:55 +0900

    test: 깨진 주문 취소 테스트 복구

--- swith-me/swith-me-payment-api/src/test/java/project/swithme/payment/test/documentationtest/snippet/PaymentSnippet.java
+++ swith-me/swith-me-payment-api/src/test/java/project/swithme/payment/test/documentationtest/snippet/PaymentSnippet.java
@@ -17,9 +17,27 @@ import org.springframework.restdocs.snippet.Attributes;
 
 public interface PaymentSnippet {
 
-    ResponseFieldsSnippet PAYMENT_COMPLETE_RESPONSE =
+    ResponseFieldsSnippet PAYMENT_COMPLETE_RESPONSE = responseFields(
+        fieldWithPath("data.paymentId").type(NUMBER).description("결제 PK"),
+        fieldWithPath("code").type(STRING).description("응답 코드"),
+        fieldWithPath("message").type(STRING).description("응답 메시지")
+    );
+
+    QueryParametersSnippet ORDER_CREATE_QUERY_PARAMS = queryParameters(
+        parameterWithName("orderId").description("주문 PK"),
+        parameterWithName("paymentKey").description("결제 키"),
+        parameterWithName("paymentType").description("결제 타입"),
+        parameterWithName("amount").description("총 비용")
+    );
+
+    QueryParametersSnippet ORDER_CANCEL_QUERY_PARAMS = queryParameters(
+        parameterWithName("paymentKey").description("결제 키"),
+        parameterWithName("cancelReason").description("주문 취소 이유")
+    );
+
+    ResponseFieldsSnippet PAYMENT_CANCEL_COMPLETE_RESPONSE =
         responseFields(
-            fieldWithPath("data.paymentId").type(NUMBER).description("결제 PK"),
+            fieldWithPath("data").type(STRING).description("응답 데이터"),
             fieldWithPath("code").type(STRING).description("응답 코드"),
             fieldWithPath("message").type(STRING).description("응답 메시지")
         );
@@ -28,22 +46,22 @@ public interface PaymentSnippet {
         return document("{class-name}/{method-name}",
             preprocessRequest(prettyPrint()),
             preprocessResponse(prettyPrint()),
-            getQueryParametersSnippet(),
+            ORDER_CREATE_QUERY_PARAMS,
             PAYMENT_COMPLETE_RESPONSE
         );
     }
 
-    private static QueryParametersSnippet getQueryParametersSnippet() {
-        return queryParameters(
-            parameterWithName("orderId").description("주문 PK"),
-            parameterWithName("paymentKey").description("결제 키"),
-            parameterWithName("paymentType").description("결제 타입"),
-            parameterWithName("amount").description("총 비용")
+    static RestDocumentationResultHandler getOrderCancelSnippet() {
+        return document("{class-name}/{method-name}",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            ORDER_CANCEL_QUERY_PARAMS,
+            PAYMENT_CANCEL_COMPLETE_RESPONSE
         );
     }
 
     static Attributes.Attribute getAttribute(String field) {
         return Attributes.key(field)
-            .value("");
+            .value("- Must be not Null");
     }
 }
