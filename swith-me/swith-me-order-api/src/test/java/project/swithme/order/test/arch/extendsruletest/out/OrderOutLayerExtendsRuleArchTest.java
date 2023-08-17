d68c6313aa3bfc9472866d66d4b29602f87db9d5
devjun10 <devjun1023@gmail.com>
2023-08-17 18:08:04 +0900

    test: Extends Rule 추가
    - 구현체 네이밍 룰 추가

---
+++ swith-me/swith-me-order-api/src/test/java/project/swithme/order/test/arch/extendsruletest/out/OrderOutLayerExtendsRuleArchTest.java
@@ -0,0 +1,23 @@
+package project.swithme.order.test.arch.extendsruletest.out;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static project.swithme.order.test.arch.ArchTestConst.ADAPTER;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.DisplayName;
+import project.swithme.order.OrderApplication;
+import project.swithme.order.common.annotation.Description;
+import project.swithme.order.core.out.ProductClient;
+import project.swithme.order.test.arch.TestExcludeConfiguration;
+
+@DisplayName("[ArchTest] PresentationLayer 계층 테스트")
+@AnalyzeClasses(packagesOf = {
+    OrderApplication.class}, importOptions = TestExcludeConfiguration.class)
+public class OrderOutLayerExtendsRuleArchTest {
+
+    @ArchTest
+    @Description(content = "ProductClient의 구현체들은 Adapter가 클래스 이름 끝에 붙어야 한다.")
+    ArchRule orderUseCaseImplRule = classes().that().implement(ProductClient.class)
+        .should().haveSimpleNameEndingWith(ADAPTER);
+}
---
+++ swith-me/swith-me-order-api/src/test/java/project/swithme/order/test/arch/extendsruletest/service/OrderServiceLayerExtendsRuleArchTest.java
@@ -0,0 +1,30 @@
+package project.swithme.order.test.arch.extendsruletest.service;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static project.swithme.order.test.arch.ArchTestConst.SERVICE;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.DisplayName;
+import project.swithme.order.OrderApplication;
+import project.swithme.order.common.annotation.Description;
+import project.swithme.order.core.application.OrderCommandUseCase;
+import project.swithme.order.core.application.OrderQueryUseCase;
+import project.swithme.order.test.arch.TestExcludeConfiguration;
+
+@DisplayName("[ArchTest] PresentationLayer 계층 테스트")
+@AnalyzeClasses(packagesOf = {
+    OrderApplication.class}, importOptions = TestExcludeConfiguration.class)
+public class OrderServiceLayerExtendsRuleArchTest {
+
+    @ArchTest
+    @Description(content = "OrderUseCase의 구현체들은 Service가 클래스 이름 끝에 붙어야 한다.")
+    ArchRule orderUseCaseImplNamingRuleTest = classes().that().implement(OrderCommandUseCase.class)
+        .should().haveSimpleNameEndingWith(SERVICE);
+
+    @ArchTest
+    @Description(content = "OrderQueryUseCase의 구현체들은 Service가 클래스 이름 끝에 붙어야 한다.")
+    ArchRule orderQueryUseCaseImplNamingRuleTest = classes().that()
+        .implement(OrderQueryUseCase.class)
+        .should().haveSimpleNameEndingWith(SERVICE);
+}
