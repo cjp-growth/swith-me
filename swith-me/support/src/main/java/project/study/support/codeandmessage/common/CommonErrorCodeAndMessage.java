4128c6501874ed6d3ba4600293857d9d027ad72d
devjun10 <devjun1023@gmail.com>
2023-08-17 19:38:48 +0900

    refactor: URL_NOT_FOUND CodeAndMessage 추가
    - ExceptionHandler에 해당 예외 정의

--- swith-me/support/src/main/java/project/study/support/codeandmessage/common/CommonErrorCodeAndMessage.java
+++ swith-me/support/src/main/java/project/study/support/codeandmessage/common/CommonErrorCodeAndMessage.java
@@ -7,6 +7,7 @@ public enum CommonErrorCodeAndMessage implements CodeAndMessage {
     API_SPEC_UN_MATCHED(400, "외부 서버 API 스펙과 매핑을 확인해주세요."),
     INVALID_PATH_VARIABLE(400, "올바른 경로를 확인해주세요."),
     UN_AUTHORIZED(401, "권한이 존재하지 않습니다."),
+    URL_NOTFOUND(404, "존재하지 않는 URL 입니다."),
     INTERNAL_SERVER_ERROR(500, "서버 내부 오류입니다."),
     BAD_GATEWAY(502, "외부 서버와 통신하는 과정에서 오류가 발생했습니다.");
 
--- swith-me/swith-me-order-api/src/main/java/project/swithme/order/common/exception/GlobalExceptionHandler.java
+++ swith-me/swith-me-order-api/src/main/java/project/swithme/order/common/exception/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package project.swithme.order.common.exception;
 
 import static project.study.support.codeandmessage.common.CommonErrorCodeAndMessage.INVALID_PATH_VARIABLE;
+import static project.study.support.codeandmessage.common.CommonErrorCodeAndMessage.URL_NOTFOUND;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import project.study.support.exception.DomainException;
 import project.study.support.response.failure.ErrorResponse;
 
@@ -26,6 +28,13 @@ public class GlobalExceptionHandler {
     ) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST.value())
             .body(ErrorResponse.of(INVALID_PATH_VARIABLE));
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorResponse> resolveNoHandlerFoundException(
+        NoHandlerFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value())
+            .body(ErrorResponse.of(URL_NOTFOUND));
     }
 
     @ExceptionHandler(Exception.class)
