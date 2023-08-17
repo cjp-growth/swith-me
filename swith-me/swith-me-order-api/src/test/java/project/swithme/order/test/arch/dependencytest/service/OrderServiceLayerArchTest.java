41af635154b9443e8d9ac389f57895c104bcea87
devjun10 <devjun1023@gmail.com>
2023-08-17 18:08:57 +0900

    test: 계층 테스트 추가, 상세한 의존성 테스트 제거
    - 불필요한 의존성 체크 제거
    - 너무 상세한 의존성 테스트 제거

--- swith-me/swith-me-order-api/src/test/java/project/swithme/order/test/arch/dependencytest/service/OrderServiceLayerArchTest.java
+++ swith-me/swith-me-order-api/src/test/java/project/swithme/order/test/arch/dependencytest/service/OrderServiceLayerArchTest.java
@@ -1,35 +1,35 @@
-package project.swithme.order.test.arch.dependencytest.facade;
+package project.swithme.order.test.arch.dependencytest.service;
 
 import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 import static project.swithme.order.test.arch.ArchTestConst.FACADE_LAYER_PACKAGE;
-import static project.swithme.order.test.arch.ArchTestConst.ORDER_CREATE_CONTROLLER;
 import static project.swithme.order.test.arch.ArchTestConst.ORDER_FACADE;
 import static project.swithme.order.test.arch.ArchTestConst.ORDER_QUERY_USE_CASE;
 import static project.swithme.order.test.arch.ArchTestConst.ORDER_USE_CASE;
-import static project.swithme.order.test.arch.ArchTestConst.PRESENTATION_LAYER_PACKAGE;
 import static project.swithme.order.test.arch.ArchTestConst.QUERY_SERVICE_LAYER_PACKAGE;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.library.Architectures.LayeredArchitecture;
 import org.junit.jupiter.api.DisplayName;
 import project.swithme.order.common.annotation.ArchTestDescription;
+import project.swithme.order.test.arch.TestExcludeConfiguration;
 
-@AnalyzeClasses(packages = "project.swithme.order.core")
-@DisplayName("[ArchTest] Facade 계층 테스트")
-class FacadeLayerArchTest {
+@DisplayName("[ArchTest] Application 계층 테스트")
+@AnalyzeClasses(
+    packages = "project.swithme.order",
+    importOptions = TestExcludeConfiguration.class
+)
+class OrderServiceLayerArchTest {
 
     @ArchTest
     @ArchTestDescription(
-        target = ORDER_FACADE,
-        contents = "주문 퍼사드 계층은 주문 생성 표면 계층을 통해서만 접근할 수 있다."
+        target = {ORDER_USE_CASE, ORDER_QUERY_USE_CASE},
+        contents = "주문 애플리케이션 계층은 퍼사드 계층을 통해서만 접근할 수 있다."
     )
-    LayeredArchitecture orderFacadeLayerAccessTest = layeredArchitecture()
-        .layer(ORDER_CREATE_CONTROLLER).definedBy(PRESENTATION_LAYER_PACKAGE)
+    LayeredArchitecture applicationLayerAccessTest = layeredArchitecture()
         .layer(ORDER_FACADE).definedBy(FACADE_LAYER_PACKAGE)
         .layer(ORDER_USE_CASE).definedBy(QUERY_SERVICE_LAYER_PACKAGE)
         .layer(ORDER_QUERY_USE_CASE).definedBy(QUERY_SERVICE_LAYER_PACKAGE)
 
-        .whereLayer(ORDER_FACADE).mayOnlyBeAccessedByLayers(ORDER_CREATE_CONTROLLER)
         .whereLayer(ORDER_USE_CASE).mayOnlyBeAccessedByLayers(ORDER_FACADE)
         .whereLayer(ORDER_QUERY_USE_CASE).mayOnlyBeAccessedByLayers(ORDER_FACADE);
 }
