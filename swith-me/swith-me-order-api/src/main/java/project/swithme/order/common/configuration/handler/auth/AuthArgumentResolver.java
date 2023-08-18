3e1912cfbd53f382399c99d52ccbf54629fab499
devjun10 <devjun1023@gmail.com>
2023-08-18 12:59:09 +0900

    feat: AuthArgumentResolver 순서 추가
    - Order(1) 지정

--- swith-me/swith-me-order-api/src/main/java/project/swithme/order/common/configuration/handler/auth/AuthArgumentResolver.java
+++ swith-me/swith-me-order-api/src/main/java/project/swithme/order/common/configuration/handler/auth/AuthArgumentResolver.java
@@ -1,6 +1,7 @@
 package project.swithme.order.common.configuration.handler.auth;
 
 import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -8,6 +9,7 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 import project.swithme.domain.common.StudyWithMeUser;
 import project.swithme.order.common.annotation.auth.LoginUser;
 
+@Order(1)
 public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
