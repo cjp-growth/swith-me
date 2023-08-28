2933c4fc2e3aebb5a82def2a0e8af33bf7fade07
devjun10 <devjun1023@gmail.com>
2023-08-28 17:00:13 +0900

    test: 알림 저장 통합 테스트 작성
    - 테스트 컨테이너 적용

---
+++ swith-me/swith-me-notification-api/src/test/java/project/swithme/notification/test/IntegrationTestBase.java
@@ -0,0 +1,52 @@
+package project.swithme.notification.test;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import project.swithme.notification.core.model.repository.NotificationRepository;
+
+@Testcontainers
+@EmbeddedKafka(ports = 0)
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public abstract class IntegrationTestBase {
+
+    @Container
+    protected static MongoDBContainer mongoDBContainer =
+        new MongoDBContainer("mongo:6.0")
+            .withExposedPorts(27017);
+
+    @Autowired
+    protected NotificationRepository notificationRepository;
+
+    @LocalServerPort
+    protected int port;
+
+    protected IntegrationTestBase() {
+    }
+
+    @DynamicPropertySource
+    static void mongoDbProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        mongoDBContainer.start();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        mongoDBContainer.close();
+    }
+}
