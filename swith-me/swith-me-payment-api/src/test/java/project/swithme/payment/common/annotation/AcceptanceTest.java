26dc2cb5d4376909c5031c1a2e0fec9f823fa7d9
devjun10 <devjun1023@gmail.com>
2023-09-18 18:04:09 +0900

    test: 테스트 어노테이션 설정 변경

---
+++ swith-me/swith-me-payment-api/src/test/java/project/swithme/payment/common/annotation/AcceptanceTest.java
@@ -0,0 +1,19 @@
+package project.swithme.payment.common.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.restdocs.RestDocumentationExtension;
+
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(RestDocumentationExtension.class)
+public @interface AcceptanceTest {
+
+}
--- swith-me/swith-me-payment-api/src/test/java/project/swithme/payment/common/annotation/IntegrationTest.java
+++ swith-me/swith-me-payment-api/src/test/java/project/swithme/payment/common/annotation/IntegrationTest.java
@@ -4,15 +4,12 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
-import org.springframework.restdocs.RestDocumentationExtension;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Execution(ExecutionMode.CONCURRENT)
-@ExtendWith(RestDocumentationExtension.class)
 public @interface IntegrationTest {
 
 }
